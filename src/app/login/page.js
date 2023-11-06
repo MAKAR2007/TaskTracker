84039831c5fadf10a812bd5d5de00e9187267c28
MAKAR2007 <54416209+MAKAR2007@users.noreply.github.com>
2023-11-06 21:06:16 +0300

    release v1.07

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -84,10 +84,10 @@ export default function LoginScreen() {
                 <input
                   type="email"
                   {...register("email", {
-                    required: "Please enter email",
+                    required: "Пожалуйста, введите почту",
                     pattern: {
                       value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                      message: "Please enter valid email",
+                      message: "Пожалуйста, введите корректную почту",
                     },
                   })}
                   className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-accent-600 sm:text-sm sm:leading-6"
@@ -108,10 +108,10 @@ export default function LoginScreen() {
                 <input
                   type="password"
                   {...register("password", {
-                    required: "Please enter password",
+                    required: "Пожалуйста, введите пароль",
                     minLength: {
                       value: 6,
-                      message: "password is more than 5 chars",
+                      message: "пароль должен быть длиннее 8 символов",
                     },
                   })}
                   className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-accent-600 sm:text-sm sm:leading-6"
